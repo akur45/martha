a6bb25606b5613c385ac9f8b9926cc813d713514
abdousaanyani@gmail.com <abdousaanyani@yahoo.fr>
2015-10-15 16:11:59 -0700

    mama

---
+++ src/vivian/coffee.java
@@ -0,0 +1,19 @@
+
+public class string{
+public static void  main(String args[]){
+
+	try(FileReader fr=new File Reader("E://file.text"))
+			{
+char [] a = new char[50];
+from.read(a);
+// reads the content to the array
+for(char c : a)
+System .out.print(c);
+{
+// prints the characters one by one
+}
+catch(IOExceptione);{
+e.printStackTrace();
+}
+}
+
